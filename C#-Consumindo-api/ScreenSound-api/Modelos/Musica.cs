5ec405504d1e721dea74118555108b1c58c014bb
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2023-09-14 11:06:37 -0400

    iniciei o curso de c# api

---
+++ C#-Consumindo-api/ScreenSound-api/Modelos/Musica.cs
@@ -0,0 +1,26 @@
+using System.Text.Json.Serialization;
+
+namespace ScreenSoundApi.Modelos;
+
+internal class Musica
+{
+  [JsonPropertyName("song")]
+  public string? Nome { get; set; }
+
+  [JsonPropertyName("artist")]
+  public string? Artista { get; set; }
+
+  [JsonPropertyName("duration_ms")]
+  public int Duracao { get; set; }
+
+  [JsonPropertyName("genre")]
+  public string? Genero { get; set; }
+
+  public void ExibirDetalhesDaMusica()
+  {
+    Console.WriteLine($"Artista: {Artista}");
+    Console.WriteLine($"Musica: {Nome}");
+    Console.WriteLine($"Duração em segundos: {Duracao / 1000}");
+    Console.WriteLine($"Genero musical: {Genero}");
+  }
+}
---
+++ C#-Consumindo-api/ScreenSound-api/Program.cs
@@ -0,0 +1,17 @@
+﻿using ScreenSoundApi.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+  try
+  {
+    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+
+    var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+    musicas[10].ExibirDetalhesDaMusica();
+  }
+  catch (Exception ex)
+  {
+    Console.WriteLine($"Temos um problema: {ex.Message}");
+  }
+}
